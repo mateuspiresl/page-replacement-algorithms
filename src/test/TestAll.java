f3366812bb55679f41560f6e31d0d02d72cd92a2
Mateus Pires <mateusplpl@gmail.com>
2017-05-21 18:31:50 -0300

    Add new test case

--- src/test/TestAll.java
+++ src/test/TestAll.java
@@ -9,6 +9,7 @@ public class TestAll
 	{
 		assertEquals(10, FIFO.run(TestExamples.getSize(0), TestExamples.getSequence(0)));
 		assertEquals(15, FIFO.run(TestExamples.getSize(1), TestExamples.getSequence(1)));
+		assertEquals(5, FIFO.run(TestExamples.getSize(2), TestExamples.getSequence(2)));
 	}
 	
 	@Test
@@ -16,6 +17,7 @@ public class TestAll
 	{
 		assertEquals(6, OTM.run(TestExamples.getSize(0), TestExamples.getSequence(0)));
 		assertEquals(9, OTM.run(TestExamples.getSize(1), TestExamples.getSequence(1)));
+		assertEquals(5, OTM.run(TestExamples.getSize(2), TestExamples.getSequence(2)));
 	}
 	
 	@Test
@@ -23,5 +25,6 @@ public class TestAll
 	{
 		assertEquals(8, LRU.run(TestExamples.getSize(0), TestExamples.getSequence(0)));
 		assertEquals(12, LRU.run(TestExamples.getSize(1), TestExamples.getSequence(1)));
+		assertEquals(5, LRU.run(TestExamples.getSize(2), TestExamples.getSequence(2)));
 	}
 }
--- src/test/TestExamples.java
+++ src/test/TestExamples.java
@@ -1,10 +1,11 @@
 
 public class TestExamples
 {
-	private static final int[] sizes = new int[] { 4, 3 };
+	private static final int[] sizes = new int[] { 4, 3, 4 };
 	private static final Integer[][] sequences = new Integer[][] {
 			new Integer[] { 1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5 },
-			new Integer[] { 7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1 }
+			new Integer[] { 7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1 },
+			new Integer[] { 1, 2, 3, 4, 1, 5, 5, 5, 5, 5, 5, 5 }
 	};
 	
 	public static int getSize(int id) {
